541f8d299d42b5c9cdbe8151c9c091e77fbaa446
daviematz <daviematz@gmail.com>
2017-08-09 21:26:21 -0400

    figured out file location path things
    Hurray! Slowly breaking through the mess that is learning xml :dancer:

--- DnDTools/DnDTools/Static Classes/XMLEditor.cs
+++ DnDTools/DnDTools/Static Classes/XMLEditor.cs
@@ -1,17 +1,26 @@
-﻿using System.Xml;
+﻿using System.IO;
+using System.Xml;
 
 namespace DnDTools.Static_Classes
 {
     static class XMLEditor
     {
-        public static void InitializeFiles(string ContainingFolder)
+        public static void InitializeFiles(string containingFolder)
         {
             // Temporarily borrowed from stack overflow so i can learn
             // HOW THE FREAKING HECK XML EDITING WORKS ASBDGUJBSRFKJGA
 
+            //10 is the number of characters for //bin//Debug
+            string fileLocation = @"" + containingFolder.Substring(0, containingFolder.Length - 10) + "\\XMLs\\FolderSchema.xml";
             // instantiate XmlDocument and load XML from file
             XmlDocument doc = new XmlDocument();
-            doc.Load(@"D:\test.xml");
+            if(File.Exists(fileLocation))
+                doc.Load(fileLocation);
+            else
+            {
+                //throw a new exception heeere
+                //I fucked up somewhere if we got here
+            }
 
             // get a list of nodes - in this case, I'm selecting all <AID> nodes under
             // the <GroupAIDs> node - change to suit your needs
